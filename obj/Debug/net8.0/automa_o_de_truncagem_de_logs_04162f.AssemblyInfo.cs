ab6a53c18a16b5195932082faa811ff09fcf8a6a
BrunoDyas <brunodyas@users.noreply.github.com>
2026-04-27 13:10:10 -0300

    feat: entrada CLI mínima
    intent: feature

--- obj/Debug/net8.0/automa_o_de_truncagem_de_logs_04162f.AssemblyInfo.cs
+++ obj/Debug/net8.0/automa_o_de_truncagem_de_logs_04162f.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("automa_o_de_truncagem_de_logs_04162f")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9137b2e998a40c577b0aeab4d64c8e02123be72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f62cfdba23c944611c512464bd5eefe9b411a55")]
 [assembly: System.Reflection.AssemblyProductAttribute("automa_o_de_truncagem_de_logs_04162f")]
 [assembly: System.Reflection.AssemblyTitleAttribute("automa_o_de_truncagem_de_logs_04162f")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
